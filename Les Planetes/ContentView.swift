89816aba69e5a0d27a1fcd45db676347795fe6d1
Cyrille CHATEAU <cyrillechateau@icloud.com>
2024-02-10 10:33:57 +0100

    Initial Commit

---
+++ Les Planetes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Les Planetes
+//
+//  Created by Cyrille Chateau on 10/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Les Planetes/Les_PlanetesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Les_PlanetesApp.swift
+//  Les Planetes
+//
+//  Created by Cyrille Chateau on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Les_PlanetesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
